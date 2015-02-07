b4474d9972a7a18464187a4c27f80af9b53808d2
AndyNhi <nhi.atn@gmail.com>
2015-02-07 13:58:06 -0700

    firebase blog

---
+++ app/controllers/blogs.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.ArrayController.extend({
+
+  actions: {
+    saveBlog: function() {
+
+      var title = this.get('title');
+      var body = this.get('body');
+
+      var blog = this.store.createRecord('blog', {title: title, body: body});
+      this.set('title', '');
+      this.set('body', '');
+      blog.save();
+    }
+  }
+
+});
