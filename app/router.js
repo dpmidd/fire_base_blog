a310dd0a474169be555b5ae05ef4b25e0db60fa5
Dmidd87 <dmidd87@gmail.com>
2015-02-07 15:47:43 -0700

    Refactored router with new page and new action

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource("blogs",{path: "/"}, function(){
+    this.route("new",{path: "/new"});
     this.resource("blog",{path: "/:blog_id"}, function(){
     });
   });
